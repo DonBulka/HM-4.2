0f528f4e38863eff22ef4ccb85034491d85c286a
DonBulka <donbulka@yandex.ru>
2022-08-31 10:11:30 +0300

    1

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public int calculate(int waist, int growth) {
-        int result = (waist / (growth * 2));
+        int result = (waist / (growth * growth));
         return result;
 
     }
